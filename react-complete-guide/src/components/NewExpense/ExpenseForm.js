b89eb268d65529c71912e69df30b6f583bc762bb
Lazar Stojanović <lakisd98@gmail.com>
2023-03-04 13:28:25 +0100

    React guide: add multiple state to form

--- react-complete-guide/src/components/NewExpense/ExpenseForm.js
+++ react-complete-guide/src/components/NewExpense/ExpenseForm.js
@@ -1,20 +1,52 @@
+import { useState } from "react";
+
 import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
+	// const [enteredTitle, setEnterTitle] = useState("");
+	// const [enteredAmount, setEnteredAmount] = useState("");
+	// const [enteredDate, setEnteredDate] = useState("");
+	const [userInput, setUserInput] = useState({
+		enteredTitle: "",
+		enteredAmount: "",
+		enteredDate: "",
+	});
+
+	const titleChangeHandler = (e) => {
+		setUserInput({
+			...userInput,
+			enteredTitle: e.target.value,
+		});
+	};
+
+	const amountChangeHandler = (e) => {
+		setUserInput({
+			...userInput,
+			enteredAmount: e.target.value,
+		});
+	};
+
+	const dateChangeHandler = (e) => {
+		setUserInput({
+			...userInput,
+			enteredDate: e.target.value,
+		});
+	};
+
 	return (
 		<form>
 			<div className="new-expense__controls">
 				<div className="new-expense__control">
 					<label>Title</label>
-					<input type="text" />
+					<input type="text" onChange={titleChangeHandler} />
 				</div>
 				<div className="new-expense__control">
 					<label>Amount</label>
-					<input type="number" min="0.01" step="0.01" />
+					<input type="number" min="0.01" step="0.01" onChange={amountChangeHandler} />
 				</div>
 				<div className="new-expense__control">
 					<label>Date</label>
-					<input type="date" min="2019-01-01" max="2022-12-31" />
+					<input type="date" min="2019-01-01" max="2022-12-31" onChange={dateChangeHandler} />
 				</div>
 			</div>
 			<div className="new-expense__actions">
