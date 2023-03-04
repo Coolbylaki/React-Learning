06c01aaaf9eeafacc49c8639c9443c9066d944c2
Lazar Stojanović <lakisd98@gmail.com>
2023-03-04 06:53:49 +0100

    React guide: add props to ExpenseItem component

--- react-complete-guide/src/components/ExpenseItem.js
+++ react-complete-guide/src/components/ExpenseItem.js
@@ -1,12 +1,12 @@
 import "./ExpenseItem.css";
 
-function ExpenseItem() {
+function ExpenseItem(props) {
 	return (
-		<div>
-			<div>March 28th 2021</div>
-			<div>
-				<h2>Car Insurance</h2>
-				<div>$294.67</div>
+		<div className="expense-item">
+			<div>{props.date.toISOString()}</div>
+			<div className="expense-item__description">
+				<h2>{props.title}</h2>
+				<div className="expense-item__price">${props.amount}</div>
 			</div>
 		</div>
 	);
