beb36a80392a8c97bfa205468f42c06fc82fcf5b
Lazar Stojanović <lakisd98@gmail.com>
2023-03-04 05:02:25 +0100

    React guide: add ExpenseItem component and styling

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,9 +1,12 @@
+import ExpenseItem from "./components/ExpenseItem";
+
 function App() {
-  return (
-    <div>
-      <h2>Let's get started!</h2>
-    </div>
-  );
+	return (
+		<div>
+			<h2>Let's get started!</h2>
+			<ExpenseItem></ExpenseItem>
+		</div>
+	);
 }
 
 export default App;
