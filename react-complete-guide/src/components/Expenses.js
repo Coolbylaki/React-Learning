df975a9dbb611f6ae07980dc2ef2dc59c668cde0
Lazar Stojanović <lakisd98@gmail.com>
2023-03-04 09:28:42 +0100

    React guide: add Card wrapper component to minimize duplication

--- react-complete-guide/src/components/Expenses.js
+++ react-complete-guide/src/components/Expenses.js
@@ -1,16 +1,17 @@
 import ExpenseItem from "./ExpenseItem";
+import Card from "./Card";
 import "./Expenses.css";
 
 function Expenses(props) {
 	const { items } = props;
 
 	return (
-		<div className="expenses">
+		<Card className="expenses">
 			<ExpenseItem title={items[0].title} amount={items[0].amount} date={items[0].date} />
 			<ExpenseItem title={items[1].title} amount={items[1].amount} date={items[1].date} />
 			<ExpenseItem title={items[2].title} amount={items[2].amount} date={items[2].date} />
 			<ExpenseItem title={items[3].title} amount={items[3].amount} date={items[3].date} />
-		</div>
+		</Card>
 	);
 }
 
