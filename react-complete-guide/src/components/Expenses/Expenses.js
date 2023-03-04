72b531f019da2e37c3f54c33aa79eadb882ffefc
Lazar Stojanović <lakisd98@gmail.com>
2023-03-04 10:10:55 +0100

    React guide: refactor bugfix

---
+++ react-complete-guide/src/components/Expenses/Expenses.js
@@ -0,0 +1,18 @@
+import ExpenseItem from "./ExpenseItem";
+import Card from "../UI/Card";
+import "./Expenses.css";
+
+function Expenses(props) {
+	const { items } = props;
+
+	return (
+		<Card className="expenses">
+			<ExpenseItem title={items[0].title} amount={items[0].amount} date={items[0].date} />
+			<ExpenseItem title={items[1].title} amount={items[1].amount} date={items[1].date} />
+			<ExpenseItem title={items[2].title} amount={items[2].amount} date={items[2].date} />
+			<ExpenseItem title={items[3].title} amount={items[3].amount} date={items[3].date} />
+		</Card>
+	);
+}
+
+export default Expenses;
