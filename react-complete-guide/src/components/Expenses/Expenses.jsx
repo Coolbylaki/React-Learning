65f20459028827f43a040ae9027abdb4ca59d7a4
Lazar Stojanović <lakisd98@gmail.com>
2023-03-06 05:04:23 +0100

    React guide: add dynamic rendering for list of expenses

--- react-complete-guide/src/components/Expenses/Expenses.jsx
+++ react-complete-guide/src/components/Expenses/Expenses.jsx
@@ -6,7 +6,6 @@ import ExpensesFilter from "./ExpensesFilter";
 import { useState } from "react";
 
 const Expenses = (props) => {
-	const { items } = props;
 	const [filteredYear, setFilteredYear] = useState("2020");
 
 	const filterChangeHandler = (year) => {
@@ -16,10 +15,9 @@ const Expenses = (props) => {
 	return (
 		<Card className="expenses">
 			<ExpensesFilter onYearChange={filterChangeHandler} selected={filteredYear} />
-			<ExpenseItem title={items[0].title} amount={items[0].amount} date={items[0].date} />
-			<ExpenseItem title={items[1].title} amount={items[1].amount} date={items[1].date} />
-			<ExpenseItem title={items[2].title} amount={items[2].amount} date={items[2].date} />
-			<ExpenseItem title={items[3].title} amount={items[3].amount} date={items[3].date} />
+			{props.items.map((expense) => (
+				<ExpenseItem key={expense.id} title={expense.title} amount={expense.amount} date={expense.date} />
+			))}
 		</Card>
 	);
 };
