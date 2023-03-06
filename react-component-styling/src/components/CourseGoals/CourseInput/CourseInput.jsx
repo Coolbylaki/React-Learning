f9165805e2a6fae3831a7fdcea1777771e87a423
Lazar Stojanović <lakisd98@gmail.com>
2023-03-07 00:41:59 +0100

    Components styling: Update button styling to styled component

--- react-component-styling/src/components/CourseGoals/CourseInput/CourseInput.jsx
+++ react-component-styling/src/components/CourseGoals/CourseInput/CourseInput.jsx
@@ -25,13 +25,9 @@ const CourseInput = (props) => {
 
 	return (
 		<form onSubmit={formSubmitHandler}>
-			<div className="form-control">
-				<label style={{ color: !isValid ? "red" : "black" }}>Course Goal</label>
-				<input
-					type="text"
-					onChange={goalInputChangeHandler}
-					style={{ borderColor: !isValid ? "red" : "#ccc", backgroundColor: !isValid ? "salmon" : "white" }}
-				/>
+			<div className={`form-control ${!isValid ? "invalid" : ""}`}>
+				<label>Course Goal</label>
+				<input type="text" onChange={goalInputChangeHandler} className={`${!isValid ? "invalid" : ""}`} />
 			</div>
 			<Button type="submit">Add Goal</Button>
 		</form>
