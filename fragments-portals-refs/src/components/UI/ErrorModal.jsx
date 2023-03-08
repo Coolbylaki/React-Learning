6e52d84513d3c45496c343fae7604df0a78e179e
Lazar Stojanović <lakisd98@gmail.com>
2023-03-08 14:41:24 +0100

    Fragments: add portals

--- fragments-portals-refs/src/components/UI/ErrorModal.jsx
+++ fragments-portals-refs/src/components/UI/ErrorModal.jsx
@@ -1,23 +1,39 @@
+import { Fragment } from "react";
+import ReactDOM from "react-dom";
+
 import Card from "./Card";
 import Button from "./Button";
 import classes from "./ErrorModal.module.css";
-import { Fragment } from "react";
+
+const Backdrop = (props) => {
+	return <div className={classes.backdrop} onClick={props.onConfirm} />;
+};
+
+const ModalOverlay = (props) => {
+	return (
+		<Card className={classes.modal}>
+			<header className={classes.header}>
+				<h2>{props.title}</h2>
+			</header>
+			<div className={classes.content}>
+				<p>{props.message}</p>
+			</div>
+			<footer className={classes.actions}>
+				<Button onClick={props.onConfirm}>Okay</Button>
+			</footer>
+		</Card>
+	);
+};
 
 const ErrorModal = (props) => {
+	// Two arguments first is the node that you want to portal, and second argument is the pointer to dom.
 	return (
 		<Fragment>
-			<div className={classes.backdrop} onClick={props.onConfirm} />
-			<Card className={classes.modal}>
-				<header className={classes.header}>
-					<h2>{props.title}</h2>
-				</header>
-				<div className={classes.content}>
-					<p>{props.message}</p>
-				</div>
-				<footer className={classes.actions}>
-					<Button onClick={props.onConfirm}>Okay</Button>
-				</footer>
-			</Card>
+			{ReactDOM.createPortal(<Backdrop onConfirm={props.onConfirm} />, document.getElementById("backdrop-root"))}
+			{ReactDOM.createPortal(
+				<ModalOverlay title={props.title} message={props.message} onConfirm={props.onConfirm} />,
+				document.getElementById("overlay-root")
+			)}
 		</Fragment>
 	);
 };
