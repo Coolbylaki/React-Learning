126059905a8b48d2f52a89170a76f3978f04c295
Lazar Stojanović <lakisd98@gmail.com>
2023-03-09 16:05:03 +0100

    useEffect: basic implementation

--- react-use-effect/src/App.jsx
+++ react-use-effect/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, Fragment } from "react";
+import { useState, Fragment, useEffect } from "react";
 
 import Login from "./components/Login/Login";
 import Home from "./components/Home/Home";
@@ -7,13 +7,23 @@ import MainHeader from "./components/MainHeader/MainHeader";
 function App() {
 	const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+	const isLoggedLocal = localStorage.getItem("isLoggedIn");
+
+	useEffect(() => {
+		if (isLoggedLocal === "1") {
+			setIsLoggedIn(true);
+		}
+	}, [isLoggedLocal]);
+
 	const loginHandler = (email, password) => {
 		// We should of course check email and password
 		// But it's just a dummy/ demo anyways
+		localStorage.setItem("isLoggedIn", "1");
 		setIsLoggedIn(true);
 	};
 
 	const logoutHandler = () => {
+		localStorage.setItem("isLoggedIn", "0");
 		setIsLoggedIn(false);
 	};
 
