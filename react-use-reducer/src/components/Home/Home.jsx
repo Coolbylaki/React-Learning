09ef39f0444cdc01cae75697fa4a9620b17cef42
Lazar Stojanović <lakisd98@gmail.com>
2023-03-10 18:07:40 +0100

    Context api: Add onLogout functionality

--- react-use-reducer/src/components/Home/Home.jsx
+++ react-use-reducer/src/components/Home/Home.jsx
@@ -1,10 +1,12 @@
 import Card from "../UI/Card/Card";
 import classes from "./Home.module.css";
+import Button from "../UI/Button/Button";
 
 const Home = (props) => {
 	return (
 		<Card className={classes.home}>
 			<h1>Welcome back!</h1>
+			<Button onClick={props.onLogout}>Logout</Button>
 		</Card>
 	);
 };
