f73bc9b6cda760247f62abcd10281f066bdb071c
Lazar Stojanović <lakisd98@gmail.com>
2023-03-14 06:10:08 +0100

    Random joke: Add page Card

--- random-joke/src/App.jsx
+++ random-joke/src/App.jsx
@@ -1,5 +1,17 @@
+import Card from "./components/UI/Card/Card";
+
 function App() {
-	return <div className="App"></div>;
+	return (
+		<div>
+			<Card>
+				<h1>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi blanditiis explicabo perspiciatis ipsum porro
+					numquam, nostrum in debitis illum obcaecati corrupti cumque repudiandae, laudantium itaque facere optio
+					voluptas tenetur. Molestias?
+				</h1>
+			</Card>
+		</div>
+	);
 }
 
 export default App;
---
+++ random-joke/src/components/UI/Card/Card.jsx
@@ -0,0 +1,7 @@
+import classes from "./Card.module.css";
+
+const Card = (props) => {
+	return <div className={`${props.className} ${classes.card}`}>{props.children}</div>;
+};
+
+export default Card;
