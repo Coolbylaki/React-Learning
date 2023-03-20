4993ba2354ef3f8ce8ba901fc1aeb97325759683
Lazar Stojanović <lakisd98@gmail.com>
2023-03-20 19:45:21 +0100

    Optimizing: add useCallback

--- use-callback-use-memo/src/App.jsx
+++ use-callback-use-memo/src/App.jsx
@@ -1,21 +1,29 @@
-import { useState } from "react";
+import { useState, useCallback } from "react";
 import Button from "./components/UI/Button/Button";
 import DemoOutput from "./components/Demo/DemoOutput";
 import "./App.css";
 
 function App() {
 	const [showParagraph, setShowParagraph] = useState(false);
+	const [allowToggle, setAllowToggle] = useState(false);
 
 	console.log("App Running!");
 
-	const toggleParagraphHandler = () => {
-		setShowParagraph((prevShowParagraph) => !prevShowParagraph);
-	};
+	const toggleParagraphHandler = useCallback(() => {
+		if (allowToggle) {
+			setShowParagraph((prevShowParagraph) => !prevShowParagraph);
+		}
+	}, [allowToggle]);
+
+	const allowToggleHandler = useCallback(() => {
+		setAllowToggle(true);
+	}, []);
 
 	return (
 		<div className="app">
 			<h1>Hi there!</h1>
-			<DemoOutput show={false} />
+			<DemoOutput show={showParagraph} />
+			<Button onClick={allowToggleHandler}>Allow Toggling!</Button>
 			<Button onClick={toggleParagraphHandler}>Toggle Paragraph!</Button>
 		</div>
 	);
