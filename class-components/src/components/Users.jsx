17d77d6c17cc04b6ca0c62bc67febd8c1167c17e
Lazar Stojanović <lakisd98@gmail.com>
2023-03-21 16:16:34 +0100

    Class components: convert users component to class

--- class-components/src/components/Users.jsx
+++ class-components/src/components/Users.jsx
@@ -8,27 +8,38 @@ const DUMMY_USERS = [
 	{ id: "u3", name: "Julie" },
 ];
 
-const Users = () => {
-	const [showUsers, setShowUsers] = useState(true);
+class Users extends Component {
+	constructor() {
+		super();
+		this.state = {
+			showUsers: true,
+			moreState: "Example",
+		};
+	}
 
-	const toggleUsersHandler = () => {
-		setShowUsers((curState) => !curState);
-	};
+	toggleUsersHandler() {
+		// this.setShowUsers((curState) => !curState); NOT HOW YOU DO IT!
+		this.setState((curState) => {
+			return { showUsers: !curState.showUsers };
+		});
+	}
 
-	const usersList = (
-		<ul>
-			{DUMMY_USERS.map((user) => (
-				<User key={user.id} name={user.name} />
-			))}
-		</ul>
-	);
+	render() {
+		const usersList = (
+			<ul>
+				{DUMMY_USERS.map((user) => (
+					<User key={user.id} name={user.name} />
+				))}
+			</ul>
+		);
 
-	return (
-		<div className={classes.users}>
-			<button onClick={toggleUsersHandler}>{showUsers ? "Hide" : "Show"} Users</button>
-			{showUsers && usersList}
-		</div>
-	);
-};
+		return (
+			<div className={classes.users}>
+				<button onClick={this.toggleUsersHandler.bind(this)}>{this.state.showUsers ? "Hide" : "Show"} Users</button>
+				{this.state.showUsers && usersList}
+			</div>
+		);
+	}
+}
 
 export default Users;
