a2c2366264b9c02596e7d6899ec672a868e4906b
Lazar Stojanović <lakisd98@gmail.com>
2023-03-21 16:03:25 +0100

    Class components: convert user component to class

--- class-components/src/components/User.jsx
+++ class-components/src/components/User.jsx
@@ -1,7 +1,10 @@
-import classes from './User.module.css';
+import { Component } from "react";
+import classes from "./User.module.css";
 
-const User = (props) => {
-  return <li className={classes.user}>{props.name}</li>;
-};
+class User extends Component {
+	render() {
+		return <li className={classes.user}>{this.props.name}</li>;
+	}
+}
 
 export default User;
--- class-components/src/components/Users.jsx
+++ class-components/src/components/Users.jsx
@@ -1,37 +1,34 @@
-import { useState } from 'react';
-import User from './User';
-
-import classes from './Users.module.css';
+import { Component } from "react";
+import User from "./User";
+import classes from "./Users.module.css";
 
 const DUMMY_USERS = [
-  { id: 'u1', name: 'Max' },
-  { id: 'u2', name: 'Manuel' },
-  { id: 'u3', name: 'Julie' },
+	{ id: "u1", name: "Max" },
+	{ id: "u2", name: "Manuel" },
+	{ id: "u3", name: "Julie" },
 ];
 
 const Users = () => {
-  const [showUsers, setShowUsers] = useState(true);
+	const [showUsers, setShowUsers] = useState(true);
 
-  const toggleUsersHandler = () => {
-    setShowUsers((curState) => !curState);
-  };
+	const toggleUsersHandler = () => {
+		setShowUsers((curState) => !curState);
+	};
 
-  const usersList = (
-    <ul>
-      {DUMMY_USERS.map((user) => (
-        <User key={user.id} name={user.name} />
-      ))}
-    </ul>
-  );
+	const usersList = (
+		<ul>
+			{DUMMY_USERS.map((user) => (
+				<User key={user.id} name={user.name} />
+			))}
+		</ul>
+	);
 
-  return (
-    <div className={classes.users}>
-      <button onClick={toggleUsersHandler}>
-        {showUsers ? 'Hide' : 'Show'} Users
-      </button>
-      {showUsers && usersList}
-    </div>
-  );
+	return (
+		<div className={classes.users}>
+			<button onClick={toggleUsersHandler}>{showUsers ? "Hide" : "Show"} Users</button>
+			{showUsers && usersList}
+		</div>
+	);
 };
 
 export default Users;
