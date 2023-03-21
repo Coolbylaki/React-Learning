9d75f4f2f996c5c1484a48ca161346fea469c838
Lazar Stojanović <lakisd98@gmail.com>
2023-03-21 22:01:42 +0100

    Http requests: Add error messages

--- https-requests/src/App.jsx
+++ https-requests/src/App.jsx
@@ -5,22 +5,49 @@ import "./App.css";
 function App() {
 	const [movies, setMovies] = useState([]);
 	const [isLoading, setIsLoading] = useState(false);
+	const [error, setError] = useState(null);
 
 	async function fetchMovieHandler() {
 		setIsLoading(true);
-		const response = await fetch("https://swapi.dev/api/films");
-		const data = await response.json();
+		setError(null);
 
-		const transformedMovies = data.results.map((movieData) => {
-			return {
-				id: movieData.episode_id,
-				title: movieData.title,
-				openingText: movieData.opening_crawl,
-				releaseDate: movieData.release_date,
-			};
-		});
-		setMovies(transformedMovies);
+		try {
+			const response = await fetch("https://swapi.dev/api/films/");
+
+			// Use Axios instead because it will generate own error's for catch block
+			if (!response.ok) {
+				throw new Error("Something went wrong!");
+			}
+
+			const data = await response.json();
+
+			const transformedMovies = data.results.map((movieData) => {
+				return {
+					id: movieData.episode_id,
+					title: movieData.title,
+					openingText: movieData.opening_crawl,
+					releaseDate: movieData.release_date,
+				};
+			});
+			setMovies(transformedMovies);
+		} catch (error) {
+			setError(error.message);
+		}
 		setIsLoading(false);
+	}
+
+	let content = <p>Found no movies.</p>;
+
+	if (movies.length > 0) {
+		content = <MoviesList movies={movies} />;
+	}
+
+	if (error) {
+		content = <p>{error}</p>;
+	}
+
+	if (isLoading) {
+		content = <p>Loading...</p>;
 	}
 
 	return (
@@ -28,10 +55,7 @@ function App() {
 			<section>
 				<button onClick={fetchMovieHandler}>Fetch Movies</button>
 			</section>
-			<section>
-				{!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
-				{isLoading && <p>Loading...</p>}
-			</section>
+			<section>{content}</section>
 		</Fragment>
 	);
 }
