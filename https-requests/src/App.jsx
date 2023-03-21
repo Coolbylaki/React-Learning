33b60a43cb8fee3fdbcc6d38294cb0dd0a7f74de
Lazar Stojanović <lakisd98@gmail.com>
2023-03-21 18:23:14 +0100

    Http requests: Initial refactor

---
+++ https-requests/src/App.jsx
@@ -0,0 +1,33 @@
+import { Fragment } from "react";
+import MoviesList from "./components/MoviesList";
+import "./App.css";
+
+function App() {
+	const dummyMovies = [
+		{
+			id: 1,
+			title: "Some Dummy Movie",
+			openingText: "This is the opening text of the movie",
+			releaseDate: "2021-05-18",
+		},
+		{
+			id: 2,
+			title: "Some Dummy Movie 2",
+			openingText: "This is the second opening text of the movie",
+			releaseDate: "2021-05-19",
+		},
+	];
+
+	return (
+		<Fragment>
+			<section>
+				<button>Fetch Movies</button>
+			</section>
+			<section>
+				<MoviesList movies={dummyMovies} />
+			</section>
+		</Fragment>
+	);
+}
+
+export default App;
