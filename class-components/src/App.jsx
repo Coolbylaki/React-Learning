3b33c422ac55aa0d43ac9976f24d9df66822ea29
Lazar Stojanović <lakisd98@gmail.com>
2023-03-21 16:59:51 +0100

    Class components: add context

--- class-components/src/App.jsx
+++ class-components/src/App.jsx
@@ -1,14 +1,22 @@
-import { Component, Fragment } from "react";
 import UserFinder from "./components/UserFinder";
+import UsersContext from "./store/users-context";
 
-class App extends Component {
-	render() {
-		return (
-			<Fragment>
-				<UserFinder />
-			</Fragment>
-		);
-	}
+const DUMMY_USERS = [
+	{ id: "u1", name: "Max" },
+	{ id: "u2", name: "Manuel" },
+	{ id: "u3", name: "Julie" },
+];
+
+function App() {
+	const usersContext = {
+		users: DUMMY_USERS,
+	};
+
+	return (
+		<UsersContext.Provider value={usersContext}>
+			<UserFinder />
+		</UsersContext.Provider>
+	);
 }
 
 export default App;
--- class-components/src/components/UserFinder.jsx
+++ class-components/src/components/UserFinder.jsx
@@ -1,31 +1,28 @@
 import { Fragment, Component } from "react";
+import UsersContext from "../store/users-context";
 import classes from "./UserFinder.module.css";
 import Users from "./Users";
 
-const DUMMY_USERS = [
-	{ id: "u1", name: "Max" },
-	{ id: "u2", name: "Manuel" },
-	{ id: "u3", name: "Julie" },
-];
-
 class UserFinder extends Component {
+	static contextType = UsersContext;
+
 	constructor() {
 		super();
 		this.state = {
-			filteredUsers: DUMMY_USERS,
+			filteredUsers: [],
 			searchTerm: "",
 		};
 	}
 
 	componentDidMount() {
 		// Send http request...
-		// this.setState({ filteredUsers: DUMMY_USERS })
+		this.setState({ filteredUsers: this.context.users });
 	}
 
 	componentDidUpdate(prevProps, prevState) {
 		if (prevState.searchTerm !== this.state.searchTerm) {
 			this.setState({
-				filteredUsers: DUMMY_USERS.filter((user) => user.name.includes(this.state.searchTerm)),
+				filteredUsers: this.context.users.filter((user) => user.name.includes(this.state.searchTerm)),
 			});
 		}
 	}
