cadbda4f84dc9b8cb64df8732a19c7995b253c88
Lazar Stojanović <lakisd98@gmail.com>
2023-03-23 01:13:47 +0100

    Custom hooks advanced: Update useHttps hook memoization

--- custom-hooks-advanced/src/App.jsx
+++ custom-hooks-advanced/src/App.jsx
@@ -1,35 +1,31 @@
 import { useEffect, useState, Fragment } from "react";
-import useHttp from "./hooks/fetch-task";
+import useHttp from "./hooks/use-https";
 import Tasks from "./components/Tasks/Tasks";
 import NewTask from "./components/NewTask/NewTask";
 
 function App() {
 	const [tasks, setTasks] = useState([]);
 
-	const transformTasks = (tasksObject) => {
-		const loadedTasks = [];
-
-		for (const taskKey in tasksObject) {
-			loadedTasks.push({ id: taskKey, text: tasksObject[taskKey].text });
-		}
-
-		setTasks(loadedTasks);
-	};
-
-	const {
-		isLoading,
-		error,
-		sendRequest: fetchTasks,
-	} = useHttp(
-		{
-			url: "https://react-practice-5bcdb-default-rtdb.europe-west1.firebasedatabase.app/tasks.json",
-		},
-		transformTasks
-	);
+	const { isLoading, error, sendRequest: fetchTasks } = useHttp();
 
 	useEffect(() => {
-		fetchTasks();
-	}, []);
+		const transformTasks = (tasksObject) => {
+			const loadedTasks = [];
+
+			for (const taskKey in tasksObject) {
+				loadedTasks.push({ id: taskKey, text: tasksObject[taskKey].text });
+			}
+
+			setTasks(loadedTasks);
+		};
+
+		fetchTasks(
+			{
+				url: "https://react-practice-5bcdb-default-rtdb.europe-west1.firebasedatabase.app/tasks.json",
+			},
+			transformTasks
+		);
+	}, [fetchTasks]);
 
 	const taskAddHandler = (task) => {
 		setTasks((prevTasks) => prevTasks.concat(task));
