fee4775fa987d97911ed3f406d452f458195c0ac
Lazar Stojanović <lakisd98@gmail.com>
2023-03-23 01:00:48 +0100

    Custom hooks advanced: Add http hook to get tasks

--- custom-hooks-advanced/src/App.jsx
+++ custom-hooks-advanced/src/App.jsx
@@ -1,39 +1,31 @@
 import { useEffect, useState, Fragment } from "react";
-
+import useHttp from "./hooks/fetch-task";
 import Tasks from "./components/Tasks/Tasks";
 import NewTask from "./components/NewTask/NewTask";
 
 function App() {
-	const [isLoading, setIsLoading] = useState(false);
-	const [error, setError] = useState(null);
 	const [tasks, setTasks] = useState([]);
 
-	const fetchTasks = async (taskText) => {
-		setIsLoading(true);
-		setError(null);
-		try {
-			const response = await fetch(
-				"https://react-practice-5bcdb-default-rtdb.europe-west1.firebasedatabase.app/tasks.json"
-			);
+	const transformTasks = (tasksObject) => {
+		const loadedTasks = [];
 
-			if (!response.ok) {
-				throw new Error("Request failed!");
-			}
-
-			const data = await response.json();
-
-			const loadedTasks = [];
-
-			for (const taskKey in data) {
-				loadedTasks.push({ id: taskKey, text: data[taskKey].text });
-			}
-
-			setTasks(loadedTasks);
-		} catch (err) {
-			setError(err.message || "Something went wrong!");
+		for (const taskKey in tasksObject) {
+			loadedTasks.push({ id: taskKey, text: tasksObject[taskKey].text });
 		}
-		setIsLoading(false);
+
+		setTasks(loadedTasks);
 	};
+
+	const {
+		isLoading,
+		error,
+		sendRequest: fetchTasks,
+	} = useHttp(
+		{
+			url: "https://react-practice-5bcdb-default-rtdb.europe-west1.firebasedatabase.app/tasks.json",
+		},
+		transformTasks
+	);
 
 	useEffect(() => {
 		fetchTasks();
