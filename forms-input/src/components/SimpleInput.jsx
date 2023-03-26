a61adc822060ebf7bbe4c3bbed57e2c5302f1e45
Lazar Stojanović <lakisd98@gmail.com>
2023-03-26 22:47:05 +0200

    Form & User input: Update basic validation

--- forms-input/src/components/SimpleInput.jsx
+++ forms-input/src/components/SimpleInput.jsx
@@ -3,6 +3,7 @@ import { useRef, useState } from "react";
 const SimpleInput = (props) => {
 	const nameInputRef = useRef();
 	const [enteredName, setEnteredName] = useState("");
+	const [enteredNameIsValid, setEnteredNameIsValid] = useState(true);
 
 	const nameInputChangeHandler = (e) => {
 		const name = e.target.value;
@@ -13,19 +14,25 @@ const SimpleInput = (props) => {
 		e.preventDefault();
 
 		if (enteredName.trim() === "") {
+			setEnteredNameIsValid(false);
 			return;
 		}
+
+		setEnteredNameIsValid(true);
 
 		const enteredValue = nameInputRef.current.value;
 		console.log(enteredValue);
 		setEnteredName("");
 	};
 
+	const nameInputClasses = enteredNameIsValid ? "form-control" : "form-control invalid";
+
 	return (
 		<form onSubmit={formSubmissionHandler}>
-			<div className="form-control">
+			<div className={nameInputClasses}>
 				<label htmlFor="name">Your Name</label>
 				<input ref={nameInputRef} type="text" id="name" onChange={nameInputChangeHandler} value={enteredName} />
+				{!enteredNameIsValid && <p className="error-text">Name must not be empty</p>}
 			</div>
 			<div className="form-actions">
 				<button>Submit</button>
