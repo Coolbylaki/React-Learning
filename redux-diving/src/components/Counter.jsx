f265e7a6f4cec0c774d957e3a101c35166515dcc
Lazar Stojanović <lakisd98@gmail.com>
2023-04-17 20:56:50 +0200

    Redux deep dive: link redux with project and make buttons dispatch actions

--- redux-diving/src/components/Counter.jsx
+++ redux-diving/src/components/Counter.jsx
@@ -1,12 +1,28 @@
 import classes from "./Counter.module.css";
+import { useSelector, useDispatch } from "react-redux";
 
 const Counter = () => {
+	const counter = useSelector((state) => state.value);
+	const dispatch = useDispatch();
+
 	const toggleCounterHandler = () => {};
+
+	const incrementCounterHandler = () => {
+		dispatch({ type: "increment" });
+	};
+
+	const decrementCounterHandler = () => {
+		dispatch({ type: "decrement" });
+	};
 
 	return (
 		<main className={classes.counter}>
 			<h1>Redux Counter</h1>
-			<div className={classes.value}>-- COUNTER VALUE --</div>
+			<div className={classes.value}>{counter}</div>
+			<div className="counter">
+				<button onClick={incrementCounterHandler}>Increment</button>
+				<button onClick={decrementCounterHandler}>Decrement</button>
+			</div>
 			<button onClick={toggleCounterHandler}>Toggle Counter</button>
 		</main>
 	);
