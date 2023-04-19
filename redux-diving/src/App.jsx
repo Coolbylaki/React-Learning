e5cd530fdcfe700a7f355cef2726f2baaef93b31
Lazar Stojanović <lakisd98@gmail.com>
2023-04-19 22:17:07 +0200

    Redux deep dive: Setup project for further redux practice

--- redux-diving/src/App.jsx
+++ redux-diving/src/App.jsx
@@ -1,7 +1,15 @@
 import Counter from "./components/Counter";
+import Header from "./components/Header";
+import Auth from "./components/Auth";
 
 function App() {
-	return <Counter />;
+	return (
+		<>
+			<Header />
+			<Auth />
+			<Counter />
+		</>
+	);
 }
 
 export default App;
