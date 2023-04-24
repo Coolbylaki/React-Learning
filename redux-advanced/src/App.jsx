0632b445d13af87ed4f4f40bd03f95622adcbf63
Lazar Stojanović <lakisd98@gmail.com>
2023-04-25 01:02:35 +0200

    Advanced Redux: add separate actions for fetching data

--- redux-advanced/src/App.jsx
+++ redux-advanced/src/App.jsx
@@ -4,7 +4,7 @@ import Products from "./components/Shop/Products";
 import Notification from "./components/UI/Notification";
 import { useEffect, Fragment } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { sendCartData } from "./store/cart-slice";
+import { sendCartData, fetchCartData } from "./store/cart-actions";
 
 let isInitial = true;
 
@@ -17,12 +17,18 @@ function App() {
 	const cart = useSelector((state) => state.cart);
 
 	useEffect(() => {
+		dispatch(fetchCartData());
+	}, [dispatch]);
+
+	useEffect(() => {
 		if (isInitial) {
 			isInitial = false;
 			return;
 		}
 
-		dispatch(sendCartData(cart));
+		if (cart.changed) {
+			dispatch(sendCartData(cart));
+		}
 	}, [cart, dispatch]);
 
 	return (
