ec87dc54320801baf70f4f3dd1055f35cf96c7de
Lazar Stojanović <lakisd98@gmail.com>
2023-04-24 22:43:51 +0200

    Advanced Redux: add side effect to store cart inside firebase

--- redux-advanced/src/App.jsx
+++ redux-advanced/src/App.jsx
@@ -1,12 +1,20 @@
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
-
+import { useEffect } from "react";
 import { useSelector } from "react-redux";
 
 function App() {
 	const isCartVisible = useSelector((state) => state.ui.cartIsVisible);
 	const totalQuantity = useSelector((state) => state.cart.totalQuantity);
+	const cart = useSelector((state) => state.cart);
+
+	useEffect(() => {
+		fetch("https://react-practice-5bcdb-default-rtdb.europe-west1.firebasedatabase.app/cart.json", {
+			method: "PUT",
+			body: JSON.stringify(cart),
+		});
+	}, [cart]);
 
 	return (
 		<Layout>
