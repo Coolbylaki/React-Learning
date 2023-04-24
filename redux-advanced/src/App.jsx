f431b372ef150191b8ed2841184704f2f0d011e2
Lazar Stojanović <lakisd98@gmail.com>
2023-04-25 00:35:18 +0200

    Advanced Redux: refactor useEffect to redux action

--- redux-advanced/src/App.jsx
+++ redux-advanced/src/App.jsx
@@ -4,7 +4,7 @@ import Products from "./components/Shop/Products";
 import Notification from "./components/UI/Notification";
 import { useEffect, Fragment } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { uiActions } from "./store/ui-slice";
+import { sendCartData } from "./store/cart-slice";
 
 let isInitial = true;
 
@@ -17,49 +17,12 @@ function App() {
 	const cart = useSelector((state) => state.cart);
 
 	useEffect(() => {
-		const sendCartData = async () => {
-			dispatch(
-				uiActions.showNotification({
-					status: "pending",
-					title: "Sending...",
-					message: "Sending cart data!",
-				})
-			);
-			const response = await fetch(
-				"https://react-practice-5bcdb-default-rtdb.europe-west1.firebasedatabase.app/cart.json",
-				{
-					method: "PUT",
-					body: JSON.stringify(cart),
-				}
-			);
-
-			if (!response.ok) {
-				throw new Error("Sending cart data failed.");
-			}
-
-			dispatch(
-				uiActions.showNotification({
-					status: "success",
-					title: "Success...",
-					message: "Sending cart successfully!",
-				})
-			);
-		};
-
 		if (isInitial) {
 			isInitial = false;
 			return;
 		}
 
-		sendCartData().catch(() => {
-			dispatch(
-				uiActions.showNotification({
-					status: "error",
-					title: "Error...",
-					message: "Sending cart data failed!",
-				})
-			);
-		});
+		dispatch(sendCartData(cart));
 	}, [cart, dispatch]);
 
 	return (
