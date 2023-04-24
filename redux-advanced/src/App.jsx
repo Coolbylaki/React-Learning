05f8e26d5120d66afd13a849bd51dff88598b58d
Lazar Stojanović <lakisd98@gmail.com>
2023-04-24 22:57:00 +0200

    Advanced Redux: update notification to show conditionally

--- redux-advanced/src/App.jsx
+++ redux-advanced/src/App.jsx
@@ -1,26 +1,70 @@
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
-import { useEffect } from "react";
-import { useSelector } from "react-redux";
+import Notification from "./components/UI/Notification";
+import { useEffect, Fragment } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { uiActions } from "./store/ui-slice";
 
 function App() {
+	const dispatch = useDispatch();
+
 	const isCartVisible = useSelector((state) => state.ui.cartIsVisible);
 	const totalQuantity = useSelector((state) => state.cart.totalQuantity);
+	const notification = useSelector((state) => state.ui.notification);
 	const cart = useSelector((state) => state.cart);
 
 	useEffect(() => {
-		fetch("https://react-practice-5bcdb-default-rtdb.europe-west1.firebasedatabase.app/cart.json", {
-			method: "PUT",
-			body: JSON.stringify(cart),
+		const sendCartData = async () => {
+			dispatch(
+				uiActions.showNotification({
+					status: "pending",
+					title: "Sending...",
+					message: "Sending cart data!",
+				})
+			);
+			const response = await fetch(
+				"https://react-practice-5bcdb-default-rtdb.europe-west1.firebasedatabase.app/cart.json",
+				{
+					method: "PUT",
+					body: JSON.stringify(cart),
+				}
+			);
+
+			if (!response.ok) {
+				throw new Error("Sending cart data failed.");
+			}
+
+			dispatch(
+				uiActions.showNotification({
+					status: "success",
+					title: "Success...",
+					message: "Sending cart successfully!",
+				})
+			);
+		};
+
+		sendCartData().catch(() => {
+			dispatch(
+				uiActions.showNotification({
+					status: "error",
+					title: "Error...",
+					message: "Sending cart data failed!",
+				})
+			);
 		});
-	}, [cart]);
+	}, [cart, dispatch]);
 
 	return (
-		<Layout>
-			{isCartVisible && totalQuantity > 0 && <Cart />}
-			<Products />
-		</Layout>
+		<Fragment>
+			{notification && (
+				<Notification status={notification.status} title={notification.title} message={notification.message} />
+			)}
+			<Layout>
+				{isCartVisible && totalQuantity > 0 && <Cart />}
+				<Products />
+			</Layout>
+		</Fragment>
 	);
 }
 
