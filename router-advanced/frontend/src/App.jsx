d52756ccf68f3b3bc271a2d5f4059e42a6f9802a
Lazar Stojanović <lakisd98@gmail.com>
2023-04-26 01:17:53 +0200

    Router advanced: add event detail page for every event

--- router-advanced/frontend/src/App.jsx
+++ router-advanced/frontend/src/App.jsx
@@ -1,5 +1,3 @@
-// 6. Output a list of dummy events to the EventsPage
-//    Every list item should include a link to the respective EventDetailPage
 // 7. Output the ID of the selected event on the EventDetailPage
 // BONUS: Add another (nested) layout route that adds the <EventNavigation> component above all /events... page components
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
--- router-advanced/frontend/src/pages/Events.jsx
+++ router-advanced/frontend/src/pages/Events.jsx
@@ -1,5 +1,33 @@
+import { Link } from "react-router-dom";
+
+const EVENTS = [
+	{
+		id: "e1",
+		title: "Event 1",
+	},
+	{
+		id: "e2",
+		title: "Event 2",
+	},
+	{
+		id: "e3",
+		title: "Event 3",
+	},
+];
+
 const EventsPage = () => {
-	return <h1>Events Page!</h1>;
+	return (
+		<>
+			<h1>Events Page!</h1>
+			<ul>
+				{EVENTS.map((event) => (
+					<li key={event.id}>
+						<Link to={event.id}>{event.title}</Link>
+					</li>
+				))}
+			</ul>
+		</>
+	);
 };
 
 export default EventsPage;
