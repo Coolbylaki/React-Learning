320c377a00f6a01fb10d6296b213e886affdb324
Lazar Stojanović <lakisd98@gmail.com>
2023-04-25 03:23:17 +0200

    Router basics: add two basic routes

--- router-basics/src/App.jsx
+++ router-basics/src/App.jsx
@@ -1,7 +1,25 @@
-import { Fragment } from "react";
+import HomePage from "./pages/Home";
+import ProductPage from "./pages/Products";
+
+import { createBrowserRouter, RouterProvider, createRoutesFromElements, Route } from "react-router-dom";
+
+/* Older way of using router
+const routeDefinitions = createRoutesFromElements(
+	<Route>
+		<Route path="/" element={<HomePage />} />
+		<Route path="/products" element={<ProductPage />} />
+	</Route>
+);
+const router = createBrowserRouter(routeDefinitions); 
+*/
+
+const router = createBrowserRouter([
+	{ path: "/", element: <HomePage /> },
+	{ path: "/products", element: <ProductPage /> },
+]);
 
 function App() {
-	return <Fragment></Fragment>;
+	return <RouterProvider router={router} />;
 }
 
 export default App;
