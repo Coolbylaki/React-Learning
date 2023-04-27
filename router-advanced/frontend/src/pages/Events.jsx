379a26b8a5fbba114a14662f9026485c0ddde003
Lazar Stojanović <lakisd98@gmail.com>
2023-04-27 02:48:25 +0200

    Router advanced: fetch list of events from backend

--- router-advanced/frontend/src/pages/Events.jsx
+++ router-advanced/frontend/src/pages/Events.jsx
@@ -1,33 +1,38 @@
-import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
 
-const EVENTS = [
-	{
-		id: "e1",
-		title: "Event 1",
-	},
-	{
-		id: "e2",
-		title: "Event 2",
-	},
-	{
-		id: "e3",
-		title: "Event 3",
-	},
-];
+import EventsList from "../components/EventsList";
 
-const EventsPage = () => {
+function EventsPage() {
+	const [isLoading, setIsLoading] = useState(false);
+	const [fetchedEvents, setFetchedEvents] = useState();
+	const [error, setError] = useState();
+
+	useEffect(() => {
+		async function fetchEvents() {
+			setIsLoading(true);
+			const response = await fetch("http://localhost:8080/events");
+
+			if (!response.ok) {
+				setError("Fetching events failed.");
+			} else {
+				const resData = await response.json();
+				setFetchedEvents(resData.events);
+			}
+			setIsLoading(false);
+		}
+
+		fetchEvents();
+	}, []);
+
 	return (
 		<>
-			<h1>Events Page!</h1>
-			<ul>
-				{EVENTS.map((event) => (
-					<li key={event.id}>
-						<Link to={event.id}>{event.title}</Link>
-					</li>
-				))}
-			</ul>
+			<div style={{ textAlign: "center" }}>
+				{isLoading && <p>Loading...</p>}
+				{error && <p>{error}</p>}
+			</div>
+			{!isLoading && fetchedEvents && <EventsList events={fetchedEvents} />}
 		</>
 	);
-};
+}
 
 export default EventsPage;
