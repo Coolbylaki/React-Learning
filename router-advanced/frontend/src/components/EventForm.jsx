d2ac3caacc8ae52ea28df61c7623bd8cffc31445
Lazar Stojanović <lakisd98@gmail.com>
2023-05-02 03:37:55 +0200

    Router Advanced: add event subbmiting state

--- router-advanced/frontend/src/components/EventForm.jsx
+++ router-advanced/frontend/src/components/EventForm.jsx
@@ -1,9 +1,13 @@
-import { Form, useNavigate } from "react-router-dom";
+import { Form, useNavigate, useNavigation } from "react-router-dom";
 
 import classes from "./EventForm.module.css";
 
 function EventForm({ method, event }) {
 	const navigate = useNavigate();
+	const navigation = useNavigation();
+
+	const isSubmitting = navigation.state === "submitting";
+
 	function cancelHandler() {
 		navigate("..");
 	}
@@ -30,7 +34,7 @@ function EventForm({ method, event }) {
 				<button type="button" onClick={cancelHandler}>
 					Cancel
 				</button>
-				<button>Save</button>
+				<button disabled={isSubmitting}>{isSubmitting ? "Submitting..." : "Save"}</button>
 			</div>
 		</Form>
 	);
