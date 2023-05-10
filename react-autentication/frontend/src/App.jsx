4d1db63293988faca5d6155a2358357d10cd0ed5
Lazar Stojanović <lakisd98@gmail.com>
2023-05-10 19:01:06 +0200

    React Authentication: Add logout functionality

--- react-autentication/frontend/src/App.jsx
+++ react-autentication/frontend/src/App.jsx
@@ -11,6 +11,7 @@ import RootLayout from "./pages/Root";
 import { action as manipulateEventAction } from "./components/EventForm";
 import NewsletterPage, { action as newsletterAction } from "./pages/Newsletter";
 import AuthenticationPage, { action as authAction } from "./pages/Authentication";
+import { action as logoutAction } from "./pages/Logout";
 
 const router = createBrowserRouter([
 	{
@@ -61,6 +62,10 @@ const router = createBrowserRouter([
 				path: "newsletter",
 				element: <NewsletterPage />,
 				action: newsletterAction,
+			},
+			{
+				path: "logout",
+				action: logoutAction,
 			},
 		],
 	},
--- react-autentication/frontend/src/components/MainNavigation.jsx
+++ react-autentication/frontend/src/components/MainNavigation.jsx
@@ -1,4 +1,4 @@
-import { NavLink } from "react-router-dom";
+import { NavLink, Form } from "react-router-dom";
 
 import classes from "./MainNavigation.module.css";
 import NewsletterSignup from "./NewsletterSignup";
@@ -25,8 +25,13 @@ function MainNavigation() {
 					</li>
 					<li>
 						<NavLink to="/auth?mode=login" className={({ isActive }) => (isActive ? classes.active : undefined)}>
-							Auth
+							Authentication
 						</NavLink>
+					</li>
+					<li>
+						<Form action="/logout" method="post">
+							<button>Logout</button>
+						</Form>
 					</li>
 				</ul>
 			</nav>
