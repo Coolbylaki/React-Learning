48b7c268893ae844176551aac1c7ee388360ba5d
Lazar Stojanović <lakisd98@gmail.com>
2023-05-10 18:04:32 +0200

    React Authentication: Add user errors output

--- react-autentication/frontend/src/components/AuthForm.jsx
+++ react-autentication/frontend/src/components/AuthForm.jsx
@@ -1,15 +1,27 @@
-import { Form, Link, useSearchParams } from "react-router-dom";
+import { Form, Link, useSearchParams, useActionData, useNavigation } from "react-router-dom";
 
 import classes from "./AuthForm.module.css";
 
 function AuthForm() {
-	const [searchParams, setSearchParams] = useSearchParams();
+	const data = useActionData();
+	const navigation = useNavigation();
+
+	const [searchParams] = useSearchParams();
 	const isLogin = searchParams.get("mode") === "login";
+	const isSubmitting = navigation.state === "submitting";
 
 	return (
 		<>
 			<Form method="post" className={classes.form}>
 				<h1>{isLogin ? "Log in" : "Create a new user"}</h1>
+				{data && data.errors && (
+					<ul>
+						{Object.values(data.errors).map((err) => (
+							<li key={err}>{err}</li>
+						))}
+					</ul>
+				)}
+				{data && data.message && <p>{data.message}</p>}
 				<p>
 					<label htmlFor="email">Email</label>
 					<input id="email" type="email" name="email" required />
@@ -20,7 +32,7 @@ function AuthForm() {
 				</p>
 				<div className={classes.actions}>
 					<Link to={`?mode=${isLogin ? "signup" : "login"}`}>{isLogin ? "Create new user" : "Login"}</Link>
-					<button>Save</button>
+					<button disabled={isSubmitting}>{isSubmitting ? "Submitting..." : isLogin ? "Login" : "Sign up"}</button>
 				</div>
 			</Form>
 		</>
