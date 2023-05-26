6e5d364191656bae5a9c7fa8cd034412a5c0c946
Lazar Stojanović <lakisd98@gmail.com>
2023-05-26 21:46:48 +0200

    Animations: Refactor animations project

--- react-animations/src/App.js
+++ react-animations/src/App.js
@@ -7,66 +7,61 @@ import Backdrop from "./components/Backdrop/Backdrop";
 import List from "./components/List/List";
 
 class App extends Component {
-  state = {
-    modalIsOpen: false,
-    showBlock: false
-  };
+	state = {
+		modalIsOpen: false,
+		showBlock: false,
+	};
 
-  showModal = () => {
-    this.setState({ modalIsOpen: true });
-  };
+	showModal = () => {
+		this.setState({ modalIsOpen: true });
+	};
 
-  closeModal = () => {
-    this.setState({ modalIsOpen: false });
-  };
+	closeModal = () => {
+		this.setState({ modalIsOpen: false });
+	};
 
-  render() {
-    return (
-      <div className="App">
-        <h1>React Animations</h1>
-        <button
-          className="Button"
-          onClick={() =>
-            this.setState(prevState => ({ showBlock: !prevState.showBlock }))}
-        >
-          Toggle
-        </button>
-        <br />
-        <Transition
-          in={this.state.showBlock}
-          timeout={1000}
-          mountOnEnter
-          unmountOnExit
-          onEnter={() => console.log('onEnter')}
-          onEntering={() => console.log('onEntering')}
-          onEntered={() => console.log('onEntered')}
-          onExit={() => console.log('onExit')}
-          onExiting={() => console.log('onExiting')}
-          onExited={() => console.log('onExited')}
-        >
-          {state => (
-            <div
-              style={{
-                backgroundColor: "red",
-                width: 100,
-                height: 100,
-                margin: "auto",
-                transition: "opacity 1s ease-out",
-                opacity: state === "exiting" ? 0 : 1
-              }}
-            />
-          )}
-        </Transition>
-        <Modal show={this.state.modalIsOpen} closed={this.closeModal} />
-        {this.state.modalIsOpen ? <Backdrop show /> : null}
-        <button className="Button" onClick={this.showModal}>
-          Open Modal
-        </button>
-        <h3>Animating Lists</h3>
-        <List />
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="App">
+				<h1>React Animations</h1>
+				<button className="Button" onClick={() => this.setState((prevState) => ({ showBlock: !prevState.showBlock }))}>
+					Toggle
+				</button>
+				<br />
+				<Transition
+					in={this.state.showBlock}
+					timeout={1000}
+					mountOnEnter
+					unmountOnExit
+					onEnter={() => console.log("onEnter")}
+					onEntering={() => console.log("onEntering")}
+					onEntered={() => console.log("onEntered")}
+					onExit={() => console.log("onExit")}
+					onExiting={() => console.log("onExiting")}
+					onExited={() => console.log("onExited")}>
+					{(state) => (
+						<div
+							style={{
+								backgroundColor: "red",
+								width: 100,
+								height: 100,
+								margin: "auto",
+								transition: "opacity 1s ease-out",
+								opacity: state === "exiting" ? 0 : 1,
+							}}
+						/>
+					)}
+				</Transition>
+				<Modal show={this.state.modalIsOpen} closed={this.closeModal} />
+				{this.state.modalIsOpen ? <Backdrop show /> : null}
+				<button className="Button" onClick={this.showModal}>
+					Open Modal
+				</button>
+				<h3>Animating Lists</h3>
+				<List />
+			</div>
+		);
+	}
 }
 
 export default App;
