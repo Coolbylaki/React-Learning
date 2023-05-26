01c517c098eb2b186eac5a01140c6e1b913c417c
Lazar Stojanović <lakisd98@gmail.com>
2023-05-27 01:11:48 +0200

    Replace Redux: add context instead of redux

--- replace-redux-with-hooks/src/components/Products/ProductItem.jsx
+++ replace-redux-with-hooks/src/components/Products/ProductItem.jsx
@@ -1,15 +1,15 @@
-import React from "react";
-import { useDispatch } from "react-redux";
+import { ProductsContext } from "../../context/products-context";
+import { useContext } from "react";
 
 import Card from "../UI/Card";
 import "./ProductItem.css";
-import { toggleFav } from "../../store/actions/products";
 
 const ProductItem = (props) => {
-	const dispatch = useDispatch();
+	const toggleFav = useContext(ProductsContext).toggleFav;
 
 	const toggleFavHandler = () => {
-		dispatch(toggleFav(props.id));
+		// dispatch(toggleFav(props.id));
+		toggleFav(props.id);
 	};
 
 	return (
