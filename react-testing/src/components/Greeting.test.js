eac6ac753e4a1decc942afd9c5a6578ad3b5a019
Lazar Stojanović <lakisd98@gmail.com>
2023-06-02 04:31:31 +0200

    React testing: writing our first component suite

--- react-testing/src/components/Greeting.test.js
+++ react-testing/src/components/Greeting.test.js
@@ -1,16 +1,57 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, act } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import Greeting from "./Greeting";
 
-test("renders Hello World as a text", () => {
-	// Arrange
-	render(<Greeting />);
+describe("Greeting component", () => {
+	test("renders Hello World as a text", () => {
+		// Arrange
+		render(<Greeting />);
 
-	// Act
-	// ... nothing
+		// Act
+		// ... nothing
 
-	// Assert
-	const helloWorldElement = screen.getByText("Hello World", {
-		exact: false,
+		// Assert
+		const helloWorldElement = screen.getByText("Hello World", {
+			exact: false,
+		});
+		expect(helloWorldElement).toBeInTheDocument();
 	});
-	expect(helloWorldElement).toBeInTheDocument();
+
+	test("renders 'It's good to see you!' if the button was NOT clicked", () => {
+		// Arrange
+		render(<Greeting />);
+
+		// Act
+		// ... nothing
+
+		// Assert
+		const outputElement = screen.getByText("good to see you", { exact: false });
+		expect(outputElement).toBeInTheDocument();
+	});
+
+	test("renders 'Changed!' if the button was clicked", async () => {
+		// Arrange
+		render(<Greeting />);
+
+		// Act
+		const user = userEvent.setup();
+		await user.click(screen.getByRole("button"));
+
+		// Assert
+		const outputElement = screen.getByText("Changed!");
+		expect(outputElement).toBeInTheDocument();
+	});
+
+	test("does not render 'It's good to see you!' if button was clicked", async () => {
+		// Arrange
+		render(<Greeting />);
+
+		// Act
+		const user = userEvent.setup();
+		await user.click(screen.getByRole("button"));
+
+		// Assert
+		const outputElement = screen.queryByText("it's good to see you", { exact: false });
+		expect(outputElement).not.toBeInTheDocument();
+	});
 });
