83731798a8dc01a9bdcd60281a126821b4a10576
Lazar Stojanović <lakisd98@gmail.com>
2023-06-02 10:32:56 +0200

    Typescript: alias

--- typescript-intro/basics.ts
+++ typescript-intro/basics.ts
@@ -13,6 +13,8 @@ let userName: string = "Coolbylaki";
 let isInstructor: boolean = true;
 
 // Complex types
+
+// Array
 let hobbies: string[];
 
 hobbies = ["hello", "world", "testing"];
@@ -21,20 +23,30 @@ let mixedArray: (number | string)[];
 
 mixedArray = ["test", 23, 22, "hello"];
 
-let person: {
+// Object
+type Person = {
 	name: string;
 	age: number;
 };
+
+let person: Person;
 
 person = {
 	name: "Max",
 	age: 32,
 };
 
-let people: { name: string; age: number }[];
+let people: Person[];
 
 people = [
 	{ name: "Max", age: 32 },
 	{ name: "Joe", age: 23 },
 	{ name: "Sam", age: 54 },
 ];
+
+// Type inference
+
+let course: string | number = "React - The Complete Guide";
+
+// course = 1235; can't because it's already set to :string
+course = 12345; // Now it can because of union type
